e352dad950c52181c420490730cc1c8e8fae1c78
FN544DW <FN544DW@IN3540215W1.ey.net>
2024-09-28 14:43:13 +0530

    Second commit

---
+++ Test Suites/TS_BrowserLaunch.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_BrowserLaunch</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>96a9e7c3-94e6-4558-9a0c-fe335de6ac97</testSuiteGuid>
+   <testCaseLink>
+      <guid>56c3bca0-7286-437b-8ed3-ee22874d8328</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_BrowserLaunch</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
